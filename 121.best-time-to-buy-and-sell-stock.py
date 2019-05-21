5ac2675ecfaa300b8dcf03b4b755ea049beb8152
Yige Tao <pshdeluxe@gmail.com>
2019-05-20 21:06:44 -0400

    depth of binary tree and sell stock

---
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+        return max_profit
+
