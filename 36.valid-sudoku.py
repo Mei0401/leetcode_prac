479c4806837e2afa21f7032f5e7b42645f35eae1
Yige Tao <pshdeluxe@gmail.com>
2019-06-28 12:15:33 -0400

    jun 28, valid sudoku

--- 36.valid-sudoku.py
+++ 36.valid-sudoku.py
@@ -12,13 +12,21 @@ def check_repeat(l):
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        topL, topR= 0,2,0,2
+        topX, topY= 0,0
         for i in range(9):
             if not (check_repeat(board[i]) and check_repeat([board[y][i] for y in range(9)])):
                 return False
             square = []
-            for y in range(topL, bottomL+1):
-                for 
+            for dx in range(3):
+                for dy in range(3):
+                    square.append(board[topY+dy][topX+dx])
+            if not check_repeat(square):
+                return False
+            topX += 3
+            if topX == 9:
+                topX = 0
+                topY += 3
+        return True
             
         
 
